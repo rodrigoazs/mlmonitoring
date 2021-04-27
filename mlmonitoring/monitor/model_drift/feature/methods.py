ebcb384720c97e797647684c2df393533ec96e2b
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2021-04-27 15:00:02 -0300

    Remove temporaly pyod

--- mlmonitoring/monitor/model_drift/feature/methods.py
+++ mlmonitoring/monitor/model_drift/feature/methods.py
@@ -1,5 +1,5 @@
-from pyod.models.auto_encoder import AutoEncoder
-from pyod.models.pca import PCA
+# from pyod.models.auto_encoder import AutoEncoder
+# from pyod.models.pca import PCA
 from mlmonitoring.monitor.utils import _calculate_psi
 from mlmonitoring.monitor.utils.autoencoder import MLPRegressorAutoEncoder
 import numpy as np
@@ -14,19 +14,19 @@ def psi_drift(X_train, X_test, **kwargs):
     return np.array(drift)
 
 
-def pca_outlier_detection(X_train, X_test, **kwargs):
-    detector = PCA(**kwargs)
-    detector.fit(X_train)
-    return detector.predict_proba(X_test)[:, -1]
+# def pca_outlier_detection(X_train, X_test, **kwargs):
+#     detector = PCA(**kwargs)
+#     detector.fit(X_train)
+#     return detector.predict_proba(X_test)[:, -1]
+
+
+# def autoencoder_outlier_detection(X_train, X_test, **kwargs):
+#     detector = AutoEncoder(**kwargs)
+#     detector.fit(X_train)
+#     return detector.predict_proba(X_test)[:, -1]
 
 
 def autoencoder_outlier_detection(X_train, X_test, **kwargs):
-    detector = AutoEncoder(**kwargs)
-    detector.fit(X_train)
-    return detector.predict_proba(X_test)[:, -1]
-
-
-def scikit_autoencoder_outlier_detection(X_train, X_test, **kwargs):
     detector = MLPRegressorAutoEncoder(**kwargs)
     detector.fit(X_train)
     return detector.predict_proba(X_test)[:, -1]
